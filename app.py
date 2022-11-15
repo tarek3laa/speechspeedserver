4390aee91e656780e9bee9f3164959fba942b314
tarek3laa <tarek_3laa@hotmail>
2022-11-15 22:38:26 +0200

    read csv file from azure

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, json
 import pandas as pd
 from azure.storage.blob import BlockBlobService
+from io import BytesIO, StringIO
 
 app = Flask(__name__)
 storage_key = "UkI6p0qbJs1oYRVn0KA8x5hQiRCfs+JJXF51+gSuT4f8NRdN1jytovwHnzQIS4ajjYWZxJgaSqpQ+AStnG1I5A=="
@@ -12,10 +13,13 @@ container_name = "data"
 @app.route('/', methods=['post'])
 def hello_world():
     data = json.loads(request.data)
-    csv = pd.read_csv('data.csv')
-    csv.loc[-1] = [data['email'], data['selected_video'], data['selected_speed']]
-    output = csv.to_csv(encoding="utf-8")
     blobService = BlockBlobService(account_name=storage_account_name, account_key=storage_key)
+    stream = BytesIO()
+    blobService.get_blob_to_stream(container_name, 'data.csv', stream)
+    stream.seek(0)
+    csv = pd.read_csv(stream)
+    csv.loc[-1] = [data['email'], data['selected_video'], data['selected_speed']]
+    output = csv.to_csv(encoding="utf-8", index=False)
     blobService.create_blob_from_text(container_name, 'data.csv', output)
     return {'status_code': 200, 'msg': 'done'}
 
