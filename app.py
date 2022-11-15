7d315f6ff4677084a6fb2102efce39cb566bdb51
tarek3laa <tarek_3laa@hotmail>
2022-11-15 20:40:23 +0200

    add azure storage

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, json
 import pandas as pd
+from azure.storage.blob import BlockBlobService
 
 app = Flask(__name__)
+storage_key = "UkI6p0qbJs1oYRVn0KA8x5hQiRCfs+JJXF51+gSuT4f8NRdN1jytovwHnzQIS4ajjYWZxJgaSqpQ+AStnG1I5A=="
+storage_account_name = "speechspeed"
+connection_string = "DefaultEndpointsProtocol=https;AccountName=speechspeed;AccountKey=UkI6p0qbJs1oYRVn0KA8x5hQiRCfs+JJXF51+gSuT4f8NRdN1jytovwHnzQIS4ajjYWZxJgaSqpQ+AStnG1I5A==;EndpointSuffix=core.windows.net"
+container_name = "data"
 
 
 @app.route('/', methods=['post'])
@@ -9,7 +14,9 @@ def hello_world():
     data = json.loads(request.data)
     csv = pd.read_csv('data.csv')
     csv.loc[-1] = [data['email'], data['selected_video'], data['selected_speed']]
-    csv.to_csv('data.csv', index=False)
+    output = csv.to_csv(encoding="utf-8")
+    blobService = BlockBlobService(account_name=storage_account_name, account_key=storage_key)
+    blobService.create_blob_from_text(container_name, 'data.csv', output)
     return {'status_code': 200}
 
 
